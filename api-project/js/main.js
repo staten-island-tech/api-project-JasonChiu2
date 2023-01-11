f4db56c5b606c728b2aab104e0ccbd2a682660fe
JasonChiu2 <stemlads@gmail.com>
2023-01-11 10:22:12 -0500

    :seat:

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,10 +1,6 @@
 import "/style.css";
 import { DOM } from "../js/DOMSelectors.js";
 
-let pokemon = DOM.name.value;
-
-const URL = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
-
 async function getData(URL) {
   try {
     const response = await fetch(URL);
@@ -12,25 +8,36 @@ async function getData(URL) {
       throw new error(response);
     } else {
       const data = await response.json();
+      console.log(data);
       DOM.container.insertAdjacentHTML(
         "afterbegin",
         `  <div class="card">
-      <h2>${data.name}</h2>
-      <img src="${data.sprites.front_default}" alt="Pokemon">
+      <h2 class="name">${data.name}</h2>
+      <img src="${data.sprites.front_default}" alt="Pokemon" class="image">
+      <h3 class="abilities">${data.abilities.forEach(
+        (el) => el.ability.name
+      )}</h3>
     </div>`
       );
     }
   } catch (error) {
     console.log(error);
+    DOM.container.insertAdjacentHTML(
+      "afterbegin",
+      `<h1 class="name">ermm what the heck</h1>`
+    );
   }
 }
 
 function clear() {
   DOM.name.value = "";
+  DOM.container.innerHTML = "";
 }
 
 DOM.form.addEventListener("submit", function () {
   event.preventDefault();
+  let pokemon = DOM.name.value;
+  const URL = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
   getData(URL);
   clear();
 });
