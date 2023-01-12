a472ff57b53032aa66cdd38dcc2299bee1989287
JasonChiu2 <stemlads@gmail.com>
2023-01-12 10:17:26 -0500

    oops

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -8,24 +8,26 @@ async function getData(URL) {
       throw new error(response);
     } else {
       const data = await response.json();
+
       console.log(data);
+
       DOM.container.insertAdjacentHTML(
         "afterbegin",
         `  <div class="card">
-      <h2 class="name">${data.name}</h2>
+      <h2 class="name">${
+        data.name.charAt(0).toUpperCase() + data.name.slice(1)
+      }</h2>
       <img src="${data.sprites.front_default}" alt="Pokemon" class="image">
-      <h3 class="abilities">${data.abilities.forEach(
-        (el) => el.ability.name
-      )}</h3>
+      <ul class="abilities"></ul>
     </div>`
       );
-    }
+      quereySelector(".abilities").insertAdjacentHTML("afterbegin", data.abilities.forEach((element) => element.ability.name)
   } catch (error) {
     console.log(error);
     DOM.container.insertAdjacentHTML(
       "afterbegin",
-      `<h1 class="name">ermm what the heck</h1>`
-    );
+      `<h1 class="name">(·_·) ermm what the heck</h1>`
+    });
   }
 }
 
@@ -36,7 +38,7 @@ function clear() {
 
 DOM.form.addEventListener("submit", function () {
   event.preventDefault();
-  let pokemon = DOM.name.value;
+  let pokemon = DOM.name.value.toLowerCase();
   const URL = `https://pokeapi.co/api/v2/pokemon/${pokemon}`;
   getData(URL);
   clear();
