b6e9de3346f7417265aa87537e77f7568d83fba8
JasonChiu2 <stemlads@gmail.com>
2023-02-17 09:48:16 -0500

    mimikyu

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,6 +1,57 @@
 import "/style.css";
 import { DOM } from "../js/DOMSelectors.js";
 
+const start = `https://pokeapi.co/api/v2/pokemon/mimikyu-disguised`;
+
+async function startUp(start) {
+  try {
+    const response = await fetch(start);
+    if (response.status < 200 || response.status > 299) {
+      throw new error(response);
+    } else {
+      const data = await response.json();
+
+      DOM.container.insertAdjacentHTML(
+        "afterbegin",
+        `  <div class="card">
+      <h2 class="name">${
+        data.name.charAt(0).toUpperCase() + data.name.slice(1)
+      }</h2>
+      <img src="${data.sprites.front_default}" alt="Front of ${
+          data.name
+        }" class="image">
+      <h3 class="list">Abilities</h3>
+      <ul class="abilities"></ul>
+      <h3 class="list">Type(s)</h3>
+      <ul class="types"></ul>
+    </div>`
+      );
+      data.abilities.forEach((a) =>
+        document
+          .querySelector(".abilities")
+          .insertAdjacentHTML(
+            "afterbegin",
+            `<li class="list-item">${
+              a.ability.name.charAt(0).toUpperCase() + a.ability.name.slice(1)
+            }</li>`
+          )
+      );
+      data.types.forEach((t) =>
+        document
+          .querySelector(".types")
+          .insertAdjacentHTML(
+            "afterbegin",
+            `<li class="list-item">${
+              t.type.name.charAt(0).toUpperCase() + t.type.name.slice(1)
+            }</li>`
+          )
+      );
+    }
+  } catch (error) {}
+}
+
+startUp(start);
+
 async function getData(URL) {
   try {
     const response = await fetch(URL);
@@ -46,7 +97,6 @@ async function getData(URL) {
       );
     }
   } catch (error) {
-    console.log(error);
     DOM.container.insertAdjacentHTML(
       "afterbegin",
       `<h1 class="error">(._.) ermm what the heck</h1>`
